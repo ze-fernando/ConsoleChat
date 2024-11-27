a48e56bee84c54187b9385f4cf9c0c25119d7025
ze-fernando <zefernando.dev@gmail.com>
2024-11-27 20:16:34 -0300

    feat: client to client

--- console_chat/client.py
+++ console_chat/client.py
@@ -2,7 +2,7 @@ import websockets
 import asyncio
 
 
-async def ws_client():
+async def ws_client(name: str):
     uri = "ws://127.0.0.1:8000/ws"
     async with websockets.connect(uri) as ws:
         print("Conectando ao servidor...")
@@ -15,7 +15,8 @@ async def ws_client():
 
             await ws.send(msg)
             res = await ws.recv()
-            print(f"Server: {res}")
+            print(f"{name}: {res}")
 
-
-asyncio.run(ws_client())
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    asyncio.run(ws_client(name))
--- console_chat/server.py
+++ console_chat/server.py
@@ -1,17 +1,24 @@
-from fastapi import WebSocket, FastAPI
+from fastapi import FastAPI, WebSocket
 import uvicorn
 
 app = FastAPI()
+clients = set()
 
 
 @app.websocket("/ws")
 async def ws_endpoint(ws: WebSocket):
     await ws.accept()
-    while True:
-        data = await ws.receive_text()
-        print(data)
-        await ws.send_text(data)
-
+    clients.add(ws)
+    try:
+        while True:
+            data = await ws.receive_text()
+            for client in clients:
+                if client != ws:
+                    await client.send_text(data)
+    except Exception:
+        pass
+    finally:
+        clients.remove(ws)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
