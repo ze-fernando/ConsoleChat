2e7aec2815b127b0a956ffa2f0ccd25d88e298bf
ze-fernando <zefernando.dev@gmail.com>
2024-11-30 00:26:11 -0300

    create: config client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+string uri = "http://localhost:5001/ws";
+
+var connection = new HubConnectionBuilder()
+  .WithUrl(uri)
+  .Build();
+
+
+connection.On<string, string>("ReceiveMessage", (user, message) =>
+{
+    Console.WriteLine($"\n{user}: {message}");
+});
+
+try
+{
+    Console.WriteLine("Concetando ao servidor");
+    await connection.StartAsync();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro ao conectar ao servidor {ex.Message}");
+    return;
+}
+
+Console.Write("Digite seu nome: ");
+string? user = Console.ReadLine();
+
+while (true)
+{
+    // Console.Write("Message: ");
+    string? message = Console.ReadLine();
+
+
+    if (string.IsNullOrEmpty(message))
+        break;
+
+    try
+    {
+        await connection.InvokeAsync("SendMessage", user, message);
+    }
+    catch (Exception ex)
+    {
+
+        Console.WriteLine($"Erro ao conectar ao servidor {ex.Message}");
+        return;
+    }
+}
+
+Console.WriteLine("\nDesconectando...");
+await connection.StopAsync();
