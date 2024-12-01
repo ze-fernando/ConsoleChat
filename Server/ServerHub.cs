1d2cab1b501fe85893741aea6572bd2cb4233c02
ze-fernando <zefernando.dev@gmail.com>
2024-12-01 13:04:49 -0300

    feat: add rooms to server

--- Server/ServerHub.cs
+++ Server/ServerHub.cs
@@ -2,8 +2,20 @@ using Microsoft.AspNetCore.SignalR;
 
 public class ServerHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessage(string room, string user, string message)
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task AddRoom(string room)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, room);
+        await Clients.Group(room).SendAsync("ReceiveMessage", "Server", $"{Context.ConnectionId} entrou na sala {room}");
+    }
+
+    public async Task LeftRoom(string room)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, room);
+        await Clients.Group(room).SendAsync("ReceiveMessage", "Server", $"{Context.ConnectionId} saiu da sala {room}");
     }
 }
